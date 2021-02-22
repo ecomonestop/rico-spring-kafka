508013a8c883d1ef789424b4b6b9caf03be301c3
ecomonestop <support@ecomonestop.com>
2021-02-22 15:37:23 -0600

    RF code

--- src/main/java/com/demo/ricospringkafka/RicoSpringKafkaApplication.java
+++ src/main/java/com/demo/ricospringkafka/RicoSpringKafkaApplication.java
@@ -1,21 +1,13 @@
 package com.demo.ricospringkafka;
 
-import java.util.function.Function;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RicoSpringKafkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RicoSpringKafkaApplication.class, args);
-	}
-	
-	@Bean
-	public Function<String, String> uppercase(){
-		return v -> v.toUpperCase();
 	}
 
 }
